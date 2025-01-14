19f23760ebb7880e4d42e9885c352775154b0823
Илья <iliakekin52@gmail.com>
2025-01-14 04:42:05 +0300

    6_14

---
+++ 6_14.asm
@@ -0,0 +1,20 @@
+#a
+  addi s7, zero, 47
+
+#b
+  addi s7, zero, -349
+
+#c
+  lui   s7, 0x00001
+  addi  s7, s7, 0x4D0
+
+#d
+  lui s7, 0xDDCCD
+
+#e
+  lui   s7, 0xFEEBC
+  addi  s7, s7, 0x789
+
+#f
+  lui   s7, 0xCCAAC
+  addi  s7, s7, 0x9AB
